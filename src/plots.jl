b1eddece16e979aa8f460963602fd14cef7a6b7a
HugoLhuillier <hugo.lhu@gmail.com>
2019-06-17 17:56:29 -0400

    :white_check_mark: add plots for comparison with original code

---
+++ src/plots.jl
@@ -0,0 +1,20 @@
+function reproduce_plots(p::Param, hh::Household)
+    for k in eachindex(p.gZ)
+        for (o,t) in Dict([:c       => ("Consumption", (240,13)),
+                            :d      => ("Deposit", (250,13)),
+                            :bdot   => ("Liquid savings",(120,13)),
+                            :adot   => ("Illiquid savings",  (250,13)),
+                            :μ      => ("Stationary dist.", (352,30))])
+            if o == :μ
+                surface(p.gB[1:25], p.gA[1:20], getfield(hh, o)[1:25,1:20,k]')
+            else
+                surface(p.gB, p.gA, getfield(hh, o)[:,:,k]')
+            end
+            plot!(xlab = "Illiquid wealth", ylab = "Liquid wealth", title = t[1],
+                c = :matter, colorbar = false, camera = t[2])
+            # plots!(camera = t[2])
+            savefig(joinpath(dirname(@__FILE__),"/figs/$(o)_$k"))
+        end
+    end
+    return nothing
+end
