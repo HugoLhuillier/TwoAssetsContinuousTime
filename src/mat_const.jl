d429379249c8886cdb9d04b0e0cdb8366edec8f3
HugoLhuillier <hugo.lhu@gmail.com>
2019-07-12 15:16:29 -0400

    :lipstick: remove old code in mat_const.jl

--- src/mat_const.jl
+++ src/mat_const.jl
@@ -15,29 +15,6 @@ function B_hjb!(p::Param, hh::Household, ind::Tuple{Int64,Int64,Int64})
     return nothing
 end
 
-# function B!(p::Param, hh::Household, ind::Tuple{Int64,Int64,Int64})
-#     # make sure that we use backward difference at bmax and forward difference at bmin for deposit
-#     if ind[1] == 1
-#         hh.X[ind...]   = - min(0, hh.scB[ind...]) / p.db
-#         hh.Y[ind...]   = min(hh.scB[ind...], 0.0) / p.db -
-#             (max(hh.scF[ind...], 0.0) + max(hh.sdF[ind...], 0.0)) / p.db
-#         hh.Z[ind...]   = (max(hh.scF[ind...], 0.0) + max(hh.sdF[ind...], 0.0)) / p.db
-#     elseif ind[1] == p.nI
-#         hh.X[ind...]   = - (min(0, hh.scB[ind...]) + hh.sdB[ind...]) / p.db
-#         hh.Y[ind...]   = (min(hh.scB[ind...], 0.0) - max(hh.scF[ind...], 0.0) + hh.sdB[ind...]) / p.db
-#         hh.Z[ind...]   = max(hh.scF[ind...], 0.0) / p.db
-#     else
-#         hh.X[ind...]   = - (min(0, hh.scB[ind...]) + min(0, hh.sdB[ind...])) / p.db
-#         hh.Y[ind...]   = (min(hh.scB[ind...], 0.0) + min(hh.sdB[ind...], 0.0)) / p.db -
-#             (max(hh.scF[ind...], 0.0) + max(hh.sdF[ind...], 0.0)) / p.db
-#         hh.Z[ind...]   = (max(hh.scF[ind...], 0.0) + max(hh.sdF[ind...], 0.0)) / p.db
-#     end
-#     # put the diagonals together in a tridiagonal matrix
-#     # TODO: thing of a more clever way to compute this matrix
-#     # concB!(p,hh,k)
-#     return nothing
-# end
-
 function B_kde!(p::Param, hh::Household, ind::Tuple{Int64,Int64,Int64})
     I           = p.lindex[ind...]
     bdot        = hh.bdot[ind...]
@@ -52,17 +29,6 @@ function B_kde!(p::Param, hh::Household, ind::Tuple{Int64,Int64,Int64})
     end
     return nothing
 end
-
-# function concB!(p::Param, hh::Household, k::Integer)
-#     # do not need X[1,:,:] and Z[end,:,:]
-#     hh.X[:,:,k]     = [hh.X[2:end,:,k]; zeros(p.nJ)']
-#     hh.Z[:,:,k]     = [zeros(p.nJ)'; hh.Z[1:(end-1),:,k]]
-#     hh.B[(p.nI*p.nJ*(k-1)+1):(p.nI*p.nJ*k),(p.nI*p.nJ*(k-1)+1):(p.nI*p.nJ*k)] =
-#                         spdiagm(-1  => vcat(hh.X[:,:,k]...)[1:(end-1)],
-#                                 0   => vcat(hh.Y[:,:,k]...),
-#                                 1   => vcat(hh.Z[:,:,k]...)[2:end])
-#     return nothing
-# end
 
 function D_hjb!(p::Param, hh::Household, ind::Tuple{Int64,Int64,Int64})
     # impose negative drift / use of backward finite difference at the upper bound
@@ -79,18 +45,6 @@ function D_hjb!(p::Param, hh::Household, ind::Tuple{Int64,Int64,Int64})
         hh.D[I,I-p.nI]  = -(min(d,0) + p.gArA[end] + p.gIncA[ind[3]]) / p.da
         hh.D[I,I]       = (min(d,0) + p.gArA[ind[2]] + p.gIncA[ind[3]]) / p.da
     end
-    # if ind[2] == p.nJ
-    #     hh.Z[ind...] = 0.0
-    #     hh.Y[ind...] = (min(hh.d[ind...],0) + p.gArA[ind[2]] + p.gIncA[ind[3]]) / p.da
-    #     hh.X[ind...] = -(min(hh.d[ind...],0) + p.gArA[end] + p.gIncA[ind[3]]) / p.da
-    # else
-    #     hh.X[ind...] = - min(hh.d[ind...], 0) / p.da
-    #     hh.Y[ind...] = (min(hh.d[ind...], 0) - max.(hh.d[ind...], 0) -
-    #                     p.gArA[ind[2]] - p.gIncA[ind[3]]) / p.da
-    #     hh.Z[ind...] = (max(hh.d[ind...], 0) + p.gArA[ind[2]] + pp.gIncA[ind[3]]) / p.da
-    # end
-    # cast together in sparse matrix
-    # concD!(p,hh,k)
     return nothing
 end
 
@@ -108,20 +62,6 @@ function D_kde!(p::Param, hh::Household, ind::Tuple{Int64,Int64,Int64})
     end
     return nothing
 end
-
-# function D_kde!(p::Param, hh::Household, k::Integer)
-#     upwind_mat!(hh, hh.adot[:,:,k], p.da, k)
-#     concD!(p, hh, k)
-#     return nothing
-# end
-
-# function concD!(p::Param, hh::Household, k::Integer)
-#     hh.D[(p.nI*p.nJ*(k-1)+1):(p.nI*p.nJ*k),(p.nI*p.nJ*(k-1)+1):(p.nI*p.nJ*k)] =
-#                         spdiagm(p.nI    => vcat(hh.Z[:,1:(end-1),k]...),
-#                                 0       => vcat(hh.Y[:,:,k]...),
-#                                 -p.nI   => vcat(hh.X[:,2:end,k]...))
-#     return nothing
-# end
 
 function A!(p::Param, hh::Household)
     hh.A = hh.B + hh.D + hh.Λ
